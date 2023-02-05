fdaa74f5b9fc60b3c153650e588c67cb53015701
YuliyaTelegina <juliatel1999@gmail.com>
2023-02-05 22:24:35 +0300

    healthcheck and swagger

--- Systems/API/DSRNetSchoolAPI/Program.cs
+++ Systems/API/DSRNetSchoolAPI/Program.cs
@@ -6,16 +6,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddAppLogger();
 
-builder.Services.AddControllers();
+// Configure services
+var services = builder.Services;
+
+services.AddHttpContextAccessor();
+services.AddAppCors();
+
+services.AddAppHealthChecks();
+services.AddAppVersioning();
+services.AddAppSwagger();
+
+services.AddAppControllerAndViews();
 
 var app = builder.Build();
+
+app.UseAppHealthChecks();
+app.UseAppSwagger();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseAppControllerAndViews();
 
 app.Run();
